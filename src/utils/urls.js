681395b29d71b94129f81979c0d6494cb971fe64
cruz <edwin.cruz.v@outlook.com>
2023-12-05 22:43:33 -0600

    configurando produccion

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,13 @@
-
 // Cambiar a sus propios datos y descomentar en el .gitignore la ruta de este archivo.
-const PUERTO = 8080
-const ESQUEMA = 'http'
-const AUTORIDAD = `localhost:${PUERTO}`
+
+// const PUERTO = 8080
+// const ESQUEMA = 'http'
+// const AUTORIDAD = `localhost:${PUERTO}`
+
+// produccion: https://tt-backend.azurewebsites.net
+const ESQUEMA = 'https'
+const AUTORIDAD = "tt-backend.azurewebsites.net"
+
 
 // Aqui en adelante no moverle.
 const API = `api`
