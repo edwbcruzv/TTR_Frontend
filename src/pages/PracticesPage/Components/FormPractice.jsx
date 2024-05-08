239df1ab63196be7a03acd3a928ea2dd5a1aa81b
cruz <edwin.cruz.v@outlook.com>
2024-05-08 01:18:27 -0600

    agregando el create de practica

--- src/pages/PracticesPage/Components/FormPractice.jsx
+++ src/pages/PracticesPage/Components/FormPractice.jsx
@@ -1,6 +1,8 @@
-import { Box, Grid, TextField } from '@mui/material'
-import React, { useContext } from 'react'
+import { Box, Grid, TextField, Button } from '@mui/material'
+import React, { useContext, useState } from 'react'
 import CrudPracticaContext from '../../../context/CrudPracticaContext'
+import EditorHTML from './EditorHTML'
+import UploadMultimediaList from '../../../components/Multimedia/UploadMultimediaList'
 
 export default function FormPractice () {
   const {
@@ -30,13 +32,61 @@ export default function FormPractice () {
     deletePractica
   } = useContext(CrudPracticaContext)
 
+  const [uploadedFilesIds, setUploadedFilesIds] = useState(watch('recursosMultimedia') ? watch('recursosMultimedia') : [])
+
+  function onSubmit (data) {
+    console.log(data)
+    // console.log(errors)
+    // id de un formulario es nulo: se crea un nuevo dato
+    // if (data.id === null) {
+    //   createData(data)
+    //   console.log('se creo un dato')
+    // } else {
+    //   // si no es nulo se editara un formulario ya existente
+    //   updateData(data)
+    //   console.log('Actualizando')
+    // }
+  }
   return (
     <Box sx={{ maxWidth: 'auto', padding: '30px' }}>
-      <Grid item xs={12}><TextField fullWidth {...register('titulo', { required: { value: true, message: 'Es requerido' } })} id='titulo' label='Titulo' type='text' variant='outlined' error={errors.titulo} helperText={(errors.titulo) && errors.titulo.message} /></Grid>
-      <Grid item xs={12}><TextField fullWidth multiline rows={5} {...register('descripcion', { required: { value: true, message: 'Es requerido' } })} id='descripcion' label='Introduccion' type='text' variant='outlined' error={errors.descripcion} helperText={(errors.descripcion) && errors.descripcion.message} /></Grid>
+      <Grid
+        container
+        spacing={2}
+        direction='row'
+        justifyContent='flex-start'
+        alignItems='flex-start'
+        alignContent='stretch'
+        wrap='wrap'
+      >
 
-      <Grid item xs={10}><TextField fullWidth multiline rows={5} {...register('comentarios', { required: { value: true, message: 'Es requerido' } })} id='comentarios' label='Comentarios' type='text' variant='outlined' error={errors.comentarios} helperText={(errors.comentarios) && errors.comentarios.message} /></Grid>
+        <Grid item xs={10}><TextField fullWidth {...register('titulo', { required: { value: true, message: 'Es requerido' } })} id='titulo' label='Titulo' type='text' variant='outlined' error={errors.titulo} helperText={(errors.titulo) && errors.titulo.message} /></Grid>
+        <Grid item xs={10}><TextField fullWidth multiline rows={5} {...register('descripcion', { required: { value: true, message: 'Es requerido' } })} id='descripcion' label='Introduccion' type='text' variant='outlined' error={errors.descripcion} helperText={(errors.descripcion) && errors.descripcion.message} /></Grid>
+        <Grid item xs={10}>
 
+          <UploadMultimediaList name='recursosMultimedia' setValue={setValue} uploadedFilesIds={uploadedFilesIds} setUploadedFilesIds={setUploadedFilesIds} />
+        </Grid>
+        <Grid item xs={10}><TextField fullWidth multiline rows={5} {...register('comentarios', { required: { value: true, message: 'Es requerido' } })} id='comentarios' label='Comentarios' type='text' variant='outlined' error={errors.comentarios} helperText={(errors.comentarios) && errors.comentarios.message} /></Grid>
+        <Grid item xs={5}>
+          <Box sx={{ mb: 2 }}>
+
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={handleSubmit(onSubmit)}
+              sx={{ mt: 1, mr: 1 }}
+            >Guardar
+            </Button>
+
+          </Box>
+          <Box sx={{ mb: 2 }}>
+
+            <Button
+              variant='outlined' color='secondary'
+            >Cancelar
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
